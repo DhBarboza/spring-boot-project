45e0e8bf048c36488945ea12e87a0d9aeeff5206
Douglas Henrique <dev.dhbarboza@gmail.com>
2022-05-02 11:03:11 -0300

    Iniciando projeto e adicionando configurações de inicialização

---
+++ DDL.sql
@@ -0,0 +1,30 @@
+create schema motostore;
+
+use motostore;
+
+create user 'user'@'localhost' identified by 'pass123';
+
+grant select, delete, update on motostore.* to user@'localhost';
+
+create table usr_usuario (
+  usr_id bigint unsigned not null auto_increment,
+  usr_nome varchar(20) not null,
+  usr_senha varchar(50) not null,
+  primary key (usr_id),
+  unique key uni_usuario_nome (usr_nome)
+);
+
+create table aut_moto (
+  aut_id bigint unsigned not null auto_increment,
+  aut_nome varchar(20) not null,
+  primary key (aut_id),
+  unique key uni_aut_nome (aut_nome)
+);
+
+create table uau_usuario_moto (
+  usr_id bigint unsigned not null,
+  aut_id bigint unsigned not null,
+  primary key (usr_id, aut_id),
+  foreign key aut_usuario_fk (usr_id) references usr_usuario (usr_id),
+  foreign key aut_moto_fk (aut_id) references aut_moto (usr_id)
+);
